5f9bf25046303bb9039bf9461612e8ef0b35d1b9
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-04-08 11:40:15 +0200

    BatVbs

--- pwdG.bat
+++ pwdG.bat
@@ -5,7 +5,9 @@ cls
 goto Start
 :Start
 title Password Generator
+echo ============================================================
 echo I will make you a new Password.
-echo Plz wr
+echo Plz write the password down somewhere in case you forget it.
+echo ============================================================
 pause
 exit
