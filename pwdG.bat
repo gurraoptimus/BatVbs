83a2279452e4e21be2a26a419496acaa0d52a5ce
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-04-08 21:35:49 +0200

    BatVbs

--- pwdG.bat
+++ pwdG.bat
@@ -10,12 +10,12 @@ echo               I will make you a new Password.
 echo   Plz write the password down somewhere in case you forget it.
 echo  ============================================================
 
-echo 1) 1 Random Password
-echo 2) 5 Random Passwords
-echo 3) 10 Random Passwords
-echo Input your choice:
+echo  1) 1 Random Password
+echo  2) 5 Random Passwords
+echo  3) 10 Random Passwords
+echo  Input your choice:
 set input=
-set /p input= Choice:
+set /p input=  Choice:
 if %input%==1 goto A if NOT goto Start2
 if %input%==2 goto B if NOT goto Start2
 if %input%==3 goto C if NOT goto Start2
