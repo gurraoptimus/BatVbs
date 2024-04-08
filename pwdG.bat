e71b0ff53c66a10cb1a2bb67f02f4da4469d5ace
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-04-08 11:45:24 +0200

    BatVbs

--- pwdG.bat
+++ pwdG.bat
@@ -9,5 +9,10 @@ echo ============================================================
 echo I will make you a new Password.
 echo Plz write the password down somewhere in case you forget it.
 echo ============================================================
+echo 1) 1 Random Password
+echo 2) 5 Random Passwords
+echo 3) 10 Random Passwords
+echo Input your choice:
+set input=
 pause
 exit
