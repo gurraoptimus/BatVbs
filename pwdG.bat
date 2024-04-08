541789c6cbed52b5fc2b3e81f9c1bb2016beb4bc
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-04-08 21:38:19 +0200

    BatVbs

--- pwdG.bat
+++ pwdG.bat
@@ -5,15 +5,15 @@ cls
 goto Start
 :Start
 title Password Generator
-echo  ============================================================
-echo               I will make you a new Password.
-echo   Plz write the password down somewhere in case you forget it.
-echo  ============================================================
+echo ============================================================
+echo I will make you a new Password.
+echo Plz write the password down somewhere in case you forget it.
+echo ============================================================
 
-echo  1) 1 Random Password
-echo  2) 5 Random Passwords
-echo  3) 10 Random Passwords
-echo  Input your choice:
+echo 1) 1 Random Password
+echo 2) 5 Random Passwords
+echo 3) 10 Random Passwords
+echo Input your choice:
 set input=
 set /p input= Choice:
 if %input%==1 goto A if NOT goto Start2
@@ -21,10 +21,10 @@ if %input%==2 goto B if NOT goto Start2
 if %input%==3 goto C if NOT goto Start2
 :A
 cls
-echo  Your password is %random%
-echo  Now choose what you want to do.
-echo  1) Go back to the beginning
-echo  2) Exit
+echo Your password is %random%
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
 set input=
 set /p input= Choice:
 if %input%==1 goto Start2 if NOT goto Start 2
@@ -33,8 +33,8 @@ if %input%==2 goto Exit if NOT goto Start 2
 exit
 :B
 cls
-echo  Your 5 passwords are %random%, %random%, %random%, %random%, %random%.
-echo  Now choose what you want to do.
-echo  1) Go back to the beginning
-echo  2) Exit
+echo Your 5 passwords are %random%, %random%, %random%, %random%, %random%.
+echo Now choose what you want to do.
+echo 1) Go back to the beginning
+echo 2) Exit
 set input= 
