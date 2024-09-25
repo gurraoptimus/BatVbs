5b4bd1c63d51d056e0e0b1464ddc6d85d7976ff6
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-09-25 11:14:26 +0200

    BatVbs

--- MultiTool.bat
+++ MultiTool.bat
@@ -13,8 +13,8 @@ for /f %%A in ('"prompt $H &echo on &for %%B in (2) do rem"') do set BS=%%A
 for /f %%A in ('"prompt $H &echo on &for %%B in (3) do rem"') do set BS=%%A
 for /f %%A in ('"prompt $H &echo on &for %%B in (4) do rem"') do set BS=%%A
 for /f %%A in ('"prompt $H &echo on &for %%B in (5) do rem"') do set BS=%%A
+for /f %%A in ('"prompt $H &echo on &for %%B in (x) do rem"') do set BS=%%A
 
-for /f %%A in ('"prompt $H &echo on &for %%B in (x) do rem"') do set X=%%A
 for /f %%A in ('"prompt $H &echo on &for %%B in (1) do rem"') do set X=%%A
 for /f %%A in ('"prompt $H &echo on &for %%B in (2) do rem"') do set X=%%A
 for /f %%A in ('"prompt $H &echo on &for %%B in (3) do rem"') do set X=%%A
@@ -30,22 +30,29 @@ echo  === (2) Spotify^>  	   ==========
 echo  === (3) WinUtil^>  	   ==========
 echo  === (4) PymP^>     	   ==========
 echo  === (5) Exit^>     	   ==========
-echo  === (X) Close all^>	   ==========
+echo  === (X) Tasklist^>	   ==========
 echo. ====================================
 echo. {G} { G U R R A O P T I M U S }  {A}
 echo. ====================================
 set /p input=.%BS% ^>
+
 if /I %input% EQU 1 start chrome.exe
 if /I %input% EQU 2 start spotify.lnk
 if /I %input% EQU 3 start WinUtil.lnk
 if /I %input% EQU 4 start pymp.exe
 if /I %input% EQU 5 Exit
-set /p input=.%X%   ^>
 
-taskkill %input% EQU 1 /im chrome.exe /t /f
-taskkill %input% EQU 2 /im spotify.exe /t /f
-taskkill %input% EQU 3 /im WinUtil.exe /t /f
-taskkill %input% EQU 4 /im pymp.exe /t /f
+
+set /p input=.%X%   ^>
+if /I %input% EQU x tasklist
+
+
+
+set /p input=.%X%     ^>
+if /I %input% EQU 1 taskkill /im chrome.exe /t /f
+if /I %input% EQU 2 taskkill /im spotify.exe /t /f
+if /I %input% EQU 3 taskkill /im powershell.exe /t /f
+if /I %input% EQU 4 taskkill /im pymp.exe /t /f
 cls
 goto start
 goto eof
