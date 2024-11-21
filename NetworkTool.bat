8b10c27f1374fbed214ee7663cd6d5545b6a8439
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-21 22:24:14 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -1,10 +1,10 @@
 @echo off
 :loop
 timeout /t 1 >nul
-for /f "tokens=1 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Descripton"') do set adapter=%%a
-for /f "tokens=3 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
-for /f "tokens=4 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
 for /f "tokens=4 delims==" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
 cls
 echo.
