e67cb4659dfa93dd79a1576f7517ea5d1f5c88e9
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-21 22:45:20 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -1,4 +1,6 @@
 @echo off
+title Network States
+echo Loading Network information...
 :loop
 timeout /t 1 >nul
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
@@ -8,15 +10,15 @@ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"')
 for /f "tokens=4 delims==" %%a in ('ping -n 2 8.8.8.8 ^| find "Average"') do set ping=%%a
 cls
 echo.
-echo Network:
-echo --------
-echo SSID: %ssid%
-echo NIC: %adapter%
-echo State: %state%
-echo Signal: %signal%
+echo  Network:
+echo  --------
+echo  SSID: %ssid%
+echo  NIC: %adapter%
+echo  State: %state%
+echo  Signal: %signal%
 echo.
-echo Speed:
-echo ------
-echo %ping%
+echo  Speed:
+echo  ------
+echo  %ping%
 
 goto loop
