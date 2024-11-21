4d03e91e7ee3058b67f59f15376dd54304727e59
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-21 22:09:45 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -1,7 +1,8 @@
 @echo off
 :loop
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
-for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "D"') do set ssid=%%a
-for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID"') do set ssid=%%a
-echo %ssid%
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Descripton"') do set adapter=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "si"') do set ssid=%%a
+echo SSID:%ssid%
 pause
