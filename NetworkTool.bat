4e52d810035ab02044b0f15a3466b6ad97bcf656
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-21 23:41:26 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -1,19 +1,20 @@
 @echo off
 title Network States
-echo Loading Network information -
-echo Loading Network information \
-echo Loading Network information ^|
-echo Loading Network information -
-echo Loading Network information ^|
-echo Loading Network information /
-echo Loading Network information ^|
-echo Loading Network information -
-echo Loading Network information \
-echo Loading Network information ^|
-echo Loading Network information -
-echo Loading Network information ^|
-echo Loading Network information /
-echo Loading Network information -
+echo  Loading Network information -
+echo  Loading Network information \
+echo  Loading Network information ^|
+echo  Loading Network information -
+echo  Loading Network information ^|
+echo  Loading Network information /
+echo  Loading Network information ^|
+echo  Loading Network information -
+echo  Loading Network information \
+echo  Loading Network information ^|
+echo  Loading Network information -
+echo  Loading Network information ^|
+echo  Loading Network information /
+echo  Loading Network information -
+cls
 timeout /t 10 >nul
 :loop
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
