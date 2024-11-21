dcfe0f31af0250cec15aee25f74246fb9ad69e1a
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-21 22:38:00 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -5,11 +5,11 @@ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| 
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Descripton"') do set adapter=%%a
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
-for /f "tokens=4 delims==" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
+for /f "tokens=4 delims==" %%a in ('ping -n 2 8.8.8.8 ^| find "Average"') do set ping=%%a
 cls
 echo.
 echo Network:
-echo ---------
+echo --------
 echo SSID: %ssid%
 echo NIC: %adapter%
 echo State: %state%
@@ -17,5 +17,6 @@ echo Signal: %signal%
 echo.
 echo Speed:
 echo ------
+echo %ping%
 
 goto loop
