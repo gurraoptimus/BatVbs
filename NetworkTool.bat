049d2b7f56715c9d31209fb6eb48b68eaf391760
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-22 00:02:01 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -9,8 +9,8 @@ for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Descripto
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "State"') do set state=%%a
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Signal"') do set signal=%%a
 for /f "tokens=4 delims==" %%a in ('ping -n 2 8.8.8.8 ^| find "Average"') do set ping=%%a
-for /f "tokens=4 delims==" %%a in ('natstat -e ^| find "Bytes"') do set rbytes=%%a
-for /f "tokens=4 delims==" %%a in ('natstat -e ^| find "Bytes"') do set sbytes=%%a
+for /f "tokens=4 delims " %%a in ('natstat -e ^| find "Bytes"') do set rbytes=%%a
+for /f "tokens=4 delims " %%a in ('natstat -e ^| find "Bytes"') do set sbytes=%%a
 cls
 echo.
 echo  Network:
