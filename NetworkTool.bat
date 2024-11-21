41c3d10ef3b69d01b99f8ff6fd5c708d3c3405d6
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-21 21:55:48 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -1,3 +1,5 @@
 @echo off
 :loop
-for /f "tokens=2 delims=:" %%a in ('')
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
+echo %ssid%
+pause
