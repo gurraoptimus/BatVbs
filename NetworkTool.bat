c6cb602ffb51c47829faec6e9aff30f3a460851f
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-22 01:24:48 +0100

    BatVbs

--- NetworkTool.bat
+++ NetworkTool.bat
@@ -4,8 +4,8 @@ title Network States
 echo  Loading Network information...
 timeout /t 5 >nul
 echo  connected Network...
-cls
 timeout /t 3 >nul
+cls
 :loop
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "SSID" ^| findstr /v "BSSID"') do set ssid=%%a
 for /f "tokens=2 delims=:" %%a in ('netsh wlan show interface ^| find "Description"') do set adapter=%%a
