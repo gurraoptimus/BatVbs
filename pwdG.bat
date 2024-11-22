6e804e6750b3534b57e13cc52b684ddcd5be09db
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-11-22 03:13:31 +0100

    BatVbs

--- pwdG.bat
+++ pwdG.bat
@@ -15,7 +15,7 @@ echo  1) 1 Random Password
 echo  2) 5 Random Passwords
 echo  3) 10 Random Passwords
 echo  Input your choice:
-set input=
+set   input=
 set  /p input= Choice:
 if %input%==1 goto A if NOT goto Start2
 if %input%==2 goto B if NOT goto Start2
@@ -26,7 +26,7 @@ echo  Your password is %random%
 echo  Now choose what you want to do.
 echo  1) Go back to the beginning
 echo  2) Exit
-set input=
+set   input=
 set  /p input= Choice:
 if %input%==1 goto Start2 if NOT goto Start 2
 if %input%==2 goto Exit if NOT goto Start 2
@@ -38,7 +38,7 @@ echo  Your 5 passwords are %random%,%random%,%random%,%random%,%random%.
 echo  Now choose what you want to do.
 echo  1) Go back to the beginning
 echo  2) Exit
-set input= 
+set   input= 
 set  /p input= Choice:
 if %input%==1 goto Start2 if NOT goto Start 2
 if %input%==2 goto Exit if NOT goto Start 2
@@ -48,7 +48,7 @@ echo  Your 10 passwords are %random%,%random%,%random%,%random%,%random%,%random
 echo  Now choose what you want to do.
 echo  1) Go back to the beginning
 echo  2) Exit
-set input= 
+set   input= 
 set  /p input= Choice:
 if %input%==1 goto Start2 if NOT goto Start 2
 if %input%==2 goto Exit if NOT goto Start 2
